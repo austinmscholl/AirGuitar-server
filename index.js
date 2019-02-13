2bf3526810bba608248548ac47cd09013949814b
austinmscholl <asprof34@gmail.com>
2019-02-13 18:46:32 -0500

    init

---
+++ index.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+
+var express = require('express');
+var app = express();
+// const port = process.env.PORT || 5000;
+var user = require('./controllers/usercontroller');
+var item = require('./controllers/itemcontroller');
+var booking = require('./controllers/bookingcontroller');
+// var location = require('./controllers/locationcontroller');  // LATER
+// var owner = require('./controllers/ownercontroller');        // LATER
+
+var sequelize = require('./db');
+var bodyParser = require('body-parser');
+
+sequelize.sync();
+
+app.use(bodyParser.json());
+
+app.use(require('./middleware/headers'));
+app.use(require('./middleware/validate-session'));
+
+app.use('/api', user);
+app.use('/items', item);
+app.use('/bookings', booking);
+// app.use('/locations', location);     // LATER
+// app.use('/owners', owner);           // LATER
+
+app.get('/api/hello', (req, res) => {
+    res.send({ express: 'Hello From Express' });
+})
+
+app.listen(process.env.PORT, function(){
+    console.log(`App is listening on ${process.env.PORT}.`)
+});
